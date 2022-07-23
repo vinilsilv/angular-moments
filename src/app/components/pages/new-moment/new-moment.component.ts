c610da8b4ba976d880642e4b4116e5a292ee2801
Vincius Silva <vinilrsilv@gmail.com>
2022-07-23 08:00:50 -0300

    feat[Forms]: Add and Clear functions added to messages

--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Moment } from 'src/app/Moment';
-import { MomentService } from 'src/app/services/moment.service'; 
+import { MessagesService } from 'src/app/services/messages.service';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -10,26 +12,31 @@ import { MomentService } from 'src/app/services/moment.service';
 export class NewMomentComponent implements OnInit {
   btnText = 'Share!';
 
-  constructor(private momentService: MomentService) {}
+  constructor(
+    private momentService: MomentService,
+    private messagesService: MessagesService,
+    private router: Router,
+  ) {}
 
   ngOnInit(): void {}
 
   async createHandler(moment: Moment) {
-    const formData = new FormData()
+    const formData = new FormData();
 
-    formData.append("title", moment.title)
-    formData.append('description', moment.description)
+    formData.append('title', moment.title);
+    formData.append('description', moment.description);
 
-    if(moment.image) {
-      formData.append('image', moment.image)
+    if (moment.image) {
+      formData.append('image', moment.image);
     }
 
     // todo
 
-    await this.momentService.createMoment(formData).subscribe()
-    
-    // display msg
+    await this.momentService.createMoment(formData).subscribe();
+
+    this.messagesService.add('Moment added succesfully')
 
     // redirect
+    this.router.navigate(['/'])
   }
 }
