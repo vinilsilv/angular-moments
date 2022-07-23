5a04958f71928ad6a98d4d696f03366bf19d66e2
Vincius Silva <vinilrsilv@gmail.com>
2022-07-23 08:16:00 -0300

    feat[Home]: Getting data from API and displaying title at Home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.baseApiUrl;
 
-  constructor() { }
+  // todo serch
+
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
-  }
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
 
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = data
+      this.moments = data
+    });
+  }
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -2,19 +2,24 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Moment } from '../Moment';
+import { Response } from './Response';
 
 import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MomentService {
   private baseApiUrl = environment.baseApiUrl;
-  private apiUrl = `${this.baseApiUrl}api/moments`
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  createMoment(formData: FormData):Observable<FormData> {
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl)
+  }
+
+  createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
   }
 }
