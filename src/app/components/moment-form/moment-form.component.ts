36553e44434f409bd512604cca1510554f046e86
Vincius Silva <vinilrsilv@gmail.com>
2022-07-23 07:31:36 -0300

    feat[Forms]: Sending data to the API

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,18 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Moment } from 'src/app/Moment';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
-  styleUrls: ['./moment-form.component.scss']
+  styleUrls: ['./moment-form.component.scss'],
 })
 export class MomentFormComponent implements OnInit {
 
+  @Output() onSubmit = new EventEmitter<Moment>()
   @Input() btnText!: string;
 
   momentForm!: FormGroup;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.momentForm = new FormGroup({
@@ -31,12 +33,19 @@ export class MomentFormComponent implements OnInit {
     return this.momentForm.get('title')!;
   }
 
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+
+    this.momentForm.patchValue({ image: file });
+  }
+
   submit() {
-    if(this.momentForm.invalid) {
+    if (this.momentForm.invalid) {
       return;
     }
 
-    console.log('Form submitted')
+    console.log(this.momentForm.value);
 
+    this.onSubmit.emit(this.momentForm.value)
   }
 }
--- src/app/components/pages/new-moment/new-moment.component.ts
+++ src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service'; 
 
 @Component({
   selector: 'app-new-moment',
   templateUrl: './new-moment.component.html',
-  styleUrls: ['./new-moment.component.scss']
+  styleUrls: ['./new-moment.component.scss'],
 })
 export class NewMomentComponent implements OnInit {
+  btnText = 'Share!';
 
-  btnText = "Share!"
+  constructor(private momentService: MomentService) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async createHandler(moment: Moment) {
+    const formData = new FormData()
+
+    formData.append("title", moment.title)
+    formData.append('description', moment.description)
+
+    if(moment.image) {
+      formData.append('image', moment.image)
+    }
+
+    // todo
+
+    await this.momentService.createMoment(formData).subscribe()
+    
+    // display msg
+
+    // redirect
   }
-
 }
