113408f1b65927fac88086db180777c0e6cfc4ae
Vincius Silva <vinilrsilv@gmail.com>
2022-07-23 09:13:23 -0300

    style[Moment]: Inproved page template

--- src/app/components/pages/moment/moment.component.ts
+++ src/app/components/pages/moment/moment.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { faEdit, faTimes } from '@fortawesome/free-solid-svg-icons';
 import { Moment } from 'src/app/Moment';
 import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-moment',
@@ -10,6 +12,9 @@ import { MomentService } from 'src/app/services/moment.service';
 })
 export class MomentComponent implements OnInit {
   moment?: Moment;
+  baseApiUrl:string = environment.baseApiUrl; 
+  faTimes = faTimes;
+  faEdit = faEdit;
 
   constructor(
     private momentService: MomentService,
